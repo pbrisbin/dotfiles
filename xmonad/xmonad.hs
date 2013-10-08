c44371ee72b565d8a4e558b7fc2f909bdae56965
patrick brisbin <pbrisbin@gmail.com>
2013-10-08 14:56:34 -0400

    xmonad.hs: remove dzen, urgency via libnotify

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,43 +1,42 @@
+{-# LANGUAGE FlexibleContexts #-}
 -------------------------------------------------------------------------------
 --
 -- xmonad.hs, pbrisbin 2013
 --
 -------------------------------------------------------------------------------
 import XMonad
-import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.UrgencyHook
 import XMonad.Util.EZConfig
-import XMonad.Util.WorkspaceCompare
+import XMonad.Util.Run
+
+import qualified XMonad.StackSet as W
 
 main :: IO ()
-main = do
-    conf <- statusBar "dzen2 -p -xs 1 -ta l -fn Verdana-12 -e 'onstart=lower'"
-                dzenPP
-                    { ppHidden = pad
-                    , ppTitle  = pad . dzenColor "#bbb" "" . dzenEscape
-                    , ppLayout = const ""
-                    , ppSort   = getSortByXineramaRule
-                    }
-                toggleStrutsKey
-                $ withUrgencyHook NoUrgencyHook
-                $ defaultConfig
-                    { terminal   = "urxvtc"
-                    , manageHook = composeAll
-                        [ isFullscreen --> doFullFloat
-                        , manageHook defaultConfig
-                        ]
-                    }
-                    `additionalKeysP`
-                        [ ("M-p", spawn "x=$(yeganesh -x -- -i -fn Verdana-12) && exec $x")
-                        , ("M-q", spawn "killall dzen2; xmonad --recompile && xmonad --restart")
-                        , ("<XF86AudioRaiseVolume>", spawn "amixer sset Master 3%+")
-                        , ("<XF86AudioLowerVolume>", spawn "amixer sset Master 3%-")
-                        , ("<XF86AudioMute>",        spawn "amixer sset Master toggle")
-                        ]
+main = xmonad
+     $ withMyUrgencyHook
+     $ defaultConfig
+        { terminal   = "urxvtc"
+        , manageHook = composeAll
+            [ isFullscreen --> doFullFloat
+            , manageHook defaultConfig
+            ]
+        }
+        `additionalKeysP`
+            [ ("M-p", spawn "x=$(yeganesh -x -- -i -fn Verdana-12) && exec $x")
+            , ("<XF86AudioRaiseVolume>", spawn "amixer sset Master 3%+")
+            , ("<XF86AudioLowerVolume>", spawn "amixer sset Master 3%-")
+            , ("<XF86AudioMute>",        spawn "amixer sset Master toggle")
+            ]
 
-    xmonad conf
+withMyUrgencyHook :: LayoutClass l Window => XConfig l -> XConfig l
+withMyUrgencyHook = withUrgencyHookC LibNotifyUrgencyHook
+                  $ urgencyConfig { suppressWhen = Focused }
 
--- | The unexported X.H.DynamicLog.toggleStrutsKey
-toggleStrutsKey :: XConfig l -> (KeyMask, KeySym)
-toggleStrutsKey XConfig { modMask = modm } = (modm, xK_b)
+data LibNotifyUrgencyHook = LibNotifyUrgencyHook deriving (Read, Show)
+
+instance UrgencyHook LibNotifyUrgencyHook where
+    urgencyHook LibNotifyUrgencyHook w = do
+        ws <- gets windowset
+        whenJust (W.findTag w ws) $ \idx ->
+            safeSpawn "notify-send" ["Bell, workspace " ++ idx]
