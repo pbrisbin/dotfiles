70b3be3f47316efba98e05ded9f4ab666d5c890b
patrick brisbin <pbrisbin@gmail.com>
2014-06-25 17:31:07 -0400

    xmonad: add scratchpad, float terminal if showing mail

--- tag-gui/xmonad/xmonad.hs
+++ tag-gui/xmonad/xmonad.hs
@@ -10,25 +10,29 @@ import XMonad.Hooks.UrgencyHook
 import XMonad.Util.EZConfig
 import XMonad.Util.NamedWindows
 import XMonad.Util.Run
+import XMonad.Util.Scratchpad
 
 import qualified XMonad.StackSet as W
 
 main :: IO ()
-main = xmonad
-     $ withMyUrgencyHook
-     $ defaultConfig
-        { terminal   = "urxvtc"
-        , manageHook = composeAll
-            [ isFullscreen --> doFullFloat
-            , manageHook defaultConfig
-            ]
-        }
-        `additionalKeysP`
-            [ ("M-p", spawn "gmrun")
-            , ("<XF86AudioRaiseVolume>", spawn "nmixer up")
-            , ("<XF86AudioLowerVolume>", spawn "nmixer down")
-            , ("<XF86AudioMute>",        spawn "nmixer toggle")
-            ]
+main = do
+    let conf = withMyUrgencyHook $ defaultConfig
+            { terminal   = "urxvtc"
+            , manageHook = composeAll
+                [ isFullscreen    --> doFullFloat
+                , title =? "mail" --> doCenterFloat
+                , scratchpadManageHook (W.RationalRect 0.6 0.8 0.35 0.1)
+                , manageHook defaultConfig
+                ]
+            }
+
+    xmonad $ conf `additionalKeysP`
+        [ ("M-p", spawn "gmrun")
+        , ("M-S-s", scratchpadSpawnAction conf)
+        , ("<XF86AudioRaiseVolume>", spawn "nmixer up")
+        , ("<XF86AudioLowerVolume>", spawn "nmixer down")
+        , ("<XF86AudioMute>",        spawn "nmixer toggle")
+        ]
 
 withMyUrgencyHook :: LayoutClass l Window => XConfig l -> XConfig l
 withMyUrgencyHook = withUrgencyHookC LibNotifyUrgencyHook
