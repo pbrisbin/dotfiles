71eb8c1d861ec47f9a38a1add8e3fae3d9cfe36e
patrick brisbin <pbrisbin@gmail.com>
2014-06-30 16:17:07 -0400

    xmonad: use named scratchpads again
    - Minor whitespace fixes

--- tag-gui/xmonad/xmonad.hs
+++ tag-gui/xmonad/xmonad.hs
@@ -10,40 +10,45 @@ import XMonad.Hooks.UrgencyHook
 import XMonad.Util.EZConfig
 import XMonad.Util.NamedWindows
 import XMonad.Util.Run
-import XMonad.Util.Scratchpad
+import XMonad.Util.NamedScratchpad
 
 import qualified XMonad.StackSet as W
 
 main :: IO ()
-main = do
-    let conf = withMyUrgencyHook $ defaultConfig
-            { terminal   = "urxvtc"
-            , manageHook = composeAll
-                [ isFullscreen    --> doFullFloat
-                , title =? "mail" --> doCenterFloat
-                , scratchpadManageHook (W.RationalRect 0.6 0.8 0.35 0.1)
-                , manageHook defaultConfig
-                ]
-            }
-
-    xmonad $ conf `additionalKeysP`
-        [ ("M-p"                   , spawn "gmrun"             )
-        , ("M-S-p"                 , scratchpadSpawnAction conf)
-        , ("M-S-m"                 , spawn "mail"              )
-        , ("<XF86AudioRaiseVolume>", spawn "nmixer up"         )
-        , ("<XF86AudioLowerVolume>", spawn "nmixer down"       )
-        , ("<XF86AudioMute>"       , spawn "nmixer toggle"     )
+main = xmonad $ withMyUrgencyHook $ defaultConfig
+    { terminal = "urxvtc"
+    , manageHook = composeAll
+        [ isFullscreen --> doFullFloat
+        , namedScratchpadManageHook scratchpads
+        , manageHook defaultConfig
+        ]
+    } `additionalKeysP`
+        [ ("M-p", spawn "gmrun")
+        , ("M-S-p", namedScratchpadAction scratchpads "scratch")
+        , ("M-S-m", namedScratchpadAction scratchpads "mail")
+        , ("<XF86AudioRaiseVolume>", spawn "nmixer up")
+        , ("<XF86AudioLowerVolume>", spawn "nmixer down")
+        , ("<XF86AudioMute>", spawn "nmixer toggle")
         ]
 
+scratchpads :: [NamedScratchpad]
+scratchpads =
+    [ NS "mail" "urxvtc -e mutt" (title =? "mutt")
+        (customFloating $ W.RationalRect 0.05 0.1 0.9 0.8)
+    , NS "scratch" "urxvtc --title scratch" (title =? "scratch")
+        (customFloating $ W.RationalRect 0.6 0.8 0.35 0.1)
+    ]
+
 withMyUrgencyHook :: LayoutClass l Window => XConfig l -> XConfig l
-withMyUrgencyHook = withUrgencyHookC LibNotifyUrgencyHook
-                  $ urgencyConfig { suppressWhen = Focused }
+withMyUrgencyHook = withUrgencyHookC LibNotifyUrgencyHook $
+    urgencyConfig { suppressWhen = Focused }
 
 data LibNotifyUrgencyHook = LibNotifyUrgencyHook deriving (Read, Show)
 
 instance UrgencyHook LibNotifyUrgencyHook where
     urgencyHook LibNotifyUrgencyHook w = do
-        name     <- getName w
+        name <- getName w
         Just idx <- fmap (W.findTag w) $ gets windowset
 
-        safeSpawn "notify-send" ["-i", "emblem-important", show name, "workspace " ++ idx]
+        safeSpawn "notify-send"
+            ["-i", "emblem-important", show name, "workspace " ++ idx]
