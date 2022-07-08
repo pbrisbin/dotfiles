a3c727232631113dc2cbc181fb3a2550948bd45e
patrick brisbin <pbrisbin@gmail.com>
2022-07-08 15:43:22 -0400

    Float windows related to pass browser extension

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,3 +1,4 @@
+import Graphics.X11.Xlib.Extras
 import XMonad
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.ManageDocks
@@ -16,7 +17,9 @@ main = xmonad $ docks $ ewmh $ def
         [ manageHook def
         , className =? "Vncviewer" --> doFloat
         , className =? "zoom" --> doFloat
+        , role =? "page-info" --> doFloat
         , role =? "pop-up" --> doFloat
+        , isDialog --> doFloat
         ]
     , layoutHook = avoidStruts $ layoutHook def
     , handleEventHook = handleEventHook def <+> fullscreenEventHook
@@ -25,3 +28,18 @@ main = xmonad $ docks $ ewmh $ def
 
   where
     role = stringProperty "WM_WINDOW_ROLE"
+
+isDialog :: Query Bool
+isDialog = ask >>= \w ->
+    liftX
+        $ (\a d -> a == Just d)
+        <$> getAtomProp "_NET_WM_WINDOW_TYPE" w
+        <*> getAtom "_NET_WM_WINDOW_TYPE_DIALOG"
+
+getAtomProp :: String -> Window -> X (Maybe Atom)
+getAtomProp name w = do
+    a <- getAtom name
+    mbr <- withDisplay $ \dpy -> io $ getWindowProperty32 dpy a w
+    case mbr of
+        Just [r] -> pure $ Just $ fromIntegral r
+        _ -> pure Nothing
