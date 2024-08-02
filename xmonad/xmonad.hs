db2d006cab1ac079cec1f68a59212d8d969b4f24
patrick brisbin <pbrisbin@gmail.com>
2024-08-02 10:25:49 -0400

    [xmonad] Two-space indentation

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -6,39 +6,43 @@ import XMonad.Util.EZConfig
 
 main :: IO ()
 main =
-    xmonad
-        $ docks
-        $ disableEwmhManageDesktopViewport
-        $ ewmh
-        $ ewmhFullscreen
-        $ def
-              { terminal = "alacritty"
-              , manageHook = mconcat
-                  [ manageHook def
-                  , className =? "Vncviewer" --> doFloat
-                  , className =? "zoom" --> doFloat
-                  , role =? "page-info" --> doFloat
-                  , role =? "pop-up" --> doFloat
-                  , isDialog --> doFloat
-                  ]
-              , layoutHook = avoidStruts $ layoutHook def
-              }
-        `additionalKeysP` [ ("M-p", spawn "dmenu_run -fn 'Noto Sans-16'")
-                          , ("M-S-p", spawn "passmenu --type")
-                          ]
-    where role = stringProperty "WM_WINDOW_ROLE"
+  xmonad
+    $ docks
+    $ disableEwmhManageDesktopViewport
+    $ ewmh
+    $ ewmhFullscreen
+    $ def
+      { terminal = "alacritty"
+      , manageHook =
+          mconcat
+            [ manageHook def
+            , className =? "Vncviewer" --> doFloat
+            , className =? "zoom" --> doFloat
+            , role =? "page-info" --> doFloat
+            , role =? "pop-up" --> doFloat
+            , isDialog --> doFloat
+            ]
+      , layoutHook = avoidStruts $ layoutHook def
+      }
+      `additionalKeysP` [ ("M-p", spawn "dmenu_run -fn 'Noto Sans-16'")
+                        , ("M-S-o", spawn "passmenu-otp --type")
+                        , ("M-S-p", spawn "passmenu --type")
+                        ]
+ where
+  role = stringProperty "WM_WINDOW_ROLE"
 
 isDialog :: Query Bool
-isDialog = ask >>= \w ->
-    liftX
-        $ (\a d -> a == Just d)
+isDialog =
+  ask >>= \w ->
+    liftX $
+      (\a d -> a == Just d)
         <$> getAtomProp "_NET_WM_WINDOW_TYPE" w
         <*> getAtom "_NET_WM_WINDOW_TYPE_DIALOG"
 
 getAtomProp :: String -> Window -> X (Maybe Atom)
 getAtomProp name w = do
-    a <- getAtom name
-    mbr <- withDisplay $ \dpy -> io $ getWindowProperty32 dpy a w
-    case mbr of
-        Just [r] -> pure $ Just $ fromIntegral r
-        _ -> pure Nothing
+  a <- getAtom name
+  mbr <- withDisplay $ \dpy -> io $ getWindowProperty32 dpy a w
+  case mbr of
+    Just [r] -> pure $ Just $ fromIntegral r
+    _ -> pure Nothing
